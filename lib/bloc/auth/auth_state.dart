07b22a047dd09a653faa8b43c6fa87a342b64144
Soomali <mehmetaliguunaydin@gmail.com>
2022-04-09 22:17:20 +0300

    added auth bloc and auth states.

--- lib/bloc/auth/auth_state.dart
+++ lib/bloc/auth/auth_state.dart
@@ -4,3 +4,17 @@ part of 'auth_bloc.dart';
 abstract class AuthState {}
 
 class AuthInitial extends AuthState {}
+
+class Authenticated extends AuthState {
+  Authenticated(this.user);
+  final User user;
+}
+
+class Authenticating extends AuthState {}
+
+class UnAuthenticated extends AuthState {}
+
+class AuthError extends AuthState {
+  AuthError(this.error);
+  final String error;
+}
