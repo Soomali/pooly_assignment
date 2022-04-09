527c00f3fe808903ee01acbc12af576e610980c2
Soomali <mehmetaliguunaydin@gmail.com>
2022-04-09 21:47:48 +0300

    added signUp function

--- lib/repos/auth/auth_repository.dart
+++ lib/repos/auth/auth_repository.dart
@@ -6,23 +6,32 @@ import '../../entities/entities.dart' as entities;
 
 class AuthRepository {
   AuthRepository(this._userRepository);
+
   final UserRepository _userRepository;
   final _auth = FirebaseAuth.instance;
+  final StreamController<String> _errorStream = StreamController();
+  Stream<String> get errorStream => _errorStream.stream;
 
   Stream<entities.User?> get self =>
       _auth.authStateChanges().transform<entities.User?>(
-          StreamTransformer.fromHandlers(handleData: (data, sink) async {
-        if (data == null) {
-          sink.add(null);
-        } else {
-          final uuid = data.uid;
-          final passengerFuture = _userRepository.getPassenger(uuid);
-          final driverFuture = _userRepository.getDriver(uuid);
-          final results = await Future.wait<entities.User?>(
-              [passengerFuture, driverFuture]);
-          sink.add(results.firstWhere((element) => element != null));
-        }
-      }));
+          StreamTransformer.fromHandlers(handleData: _transform));
 
-  //Future<void> SignUp()
+  void _transform(data, sink) async {
+    if (data == null) {
+      sink.add(null);
+    } else {
+      final uuid = data.uid;
+      final passengerFuture = _userRepository.getPassenger(uuid);
+      final driverFuture = _userRepository.getDriver(uuid);
+      final results =
+          await Future.wait<entities.User?>([passengerFuture, driverFuture]);
+      sink.add(results.firstWhere((element) => element != null));
+    }
+  }
+
+  Future<void> SignUp(entities.SignUpUserData data) async {
+    try {} on FirebaseAuthException catch (e) {
+      _errorStream.add(e.code);
+    }
+  }
 }
