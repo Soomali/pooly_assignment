cdedf8998f13a89376e09b7893266db2239bd66e
Soomali <mehmetaliguunaydin@gmail.com>
2022-04-09 21:17:40 +0300

    basic implementation of the user,passenger and driver

--- lib/entities/entities.dart
+++ lib/entities/entities.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 part 'route.dart';
 part 'user.dart';
 part 'drive.dart';
 part 'passenger.dart';
+
 part 'driver.dart';
--- lib/entities/user.dart
+++ lib/entities/user.dart
@@ -1,1 +1,18 @@
 part of 'entities.dart';
+
+abstract class User {
+  final DocumentReference id;
+  final String name;
+  final String surname;
+  final int age;
+  final String university;
+  final String department;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.surname,
+      required this.age,
+      required this.university,
+      required this.department});
+}
