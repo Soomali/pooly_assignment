d2c4b3530b57ff3a5b761bced7f1d6efc936f879
Soomali <mehmetaliguunaydin@gmail.com>
2022-04-09 21:12:38 +0300

    added main directories and some empty files and added packages.

--- lib/bloc/search/search_event.dart
+++ lib/bloc/search/search_event.dart
@@ -2,3 +2,12 @@ part of 'search_bloc.dart';
 
 @immutable
 abstract class SearchEvent {}
+
+class SearchKeyChanged {
+  final String key;
+  const SearchKeyChanged(this.key);
+}
+
+class SearchDeleted {}
+
+class SearchItemSelected {}
---
+++ lib/entities/entities.dart
@@ -0,0 +1,5 @@
+part 'route.dart';
+part 'user.dart';
+part 'drive.dart';
+part 'passenger.dart';
+part 'driver.dart';
